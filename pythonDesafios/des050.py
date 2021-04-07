009901f572c73de07cb7620301f5aaa50c7fd424
Dalton Borges <DaltonBorges@live.com>
2021-04-07 03:57:08 -0300

    Desafio 050 feito

--- pythonDesafios/des050.py
+++ pythonDesafios/des050.py
@@ -1,1 +1,16 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor for ímpar, desconsidere-o.
+
+
+#-----------------------------------------------
+s = 0
+for num in range(1,7):
+    num = int(input('Digite um número: '))
+    if num % 2 == 0:
+        s += num
+print('·'*40)
+print('A soma  de todos valores \033[4mpares\033[m é \033[1;32m{}\033[m.'.format(s))
+print('·'*40)
+
+
+#* Funcionou!!
+#-----------------------------------------------
