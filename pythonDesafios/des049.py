410c935b7fe5da1da4119912edf774b81335f400
Dalton Borges <DaltonBorges@live.com>
2021-04-07 03:09:30 -0300

    Desafio 049 feito
    Desafio 049 funcionando

--- pythonDesafios/des049.py
+++ pythonDesafios/des049.py
@@ -1,1 +1,15 @@
 # Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um LAÇO FOR.
+
+#----------------------------------------------------------
+
+num = int(input('dígite um número para ver sua taubada: '))
+print('\n Tabuada do {}:'.format(num))
+print('·'*15)
+for tab in range(0, 11):
+    print('{} x {:2} = {}'.format(num, tab , num * tab))
+print('·'*15)    
+
+#* Funcionou!
+#-----------------------------------------------------------
+
+
--- pythonDesafios/teste.py
+++ pythonDesafios/teste.py
@@ -1,3 +1,14 @@
+
+# Better Comments teste em Python
+
+#* Importante
+#- Destaque
+# Todo: teste
+#! Alert
+#? Questão
+#// Riscado
+
+
 from random import randint
 lista = ('Pedra', 'Papel', 'Tesoura')
 comp = randint(0,2)
