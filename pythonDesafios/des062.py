327d2de218acfe7f00ffba032642799e6abbbe44
Dalton Borges <daltonborges@live.com>
2021-04-11 00:47:23 -0300

    Aula do vídeo #09  - Snake Game - Parte 2

--- pythonDesafios/des062.py
+++ pythonDesafios/des062.py
@@ -1,1 +1,23 @@
 # Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
+# ! A matemática já começou a foder...
+
+# ----------------------------------------
+print('·' * 20)
+print('Dez termos de P.A.')
+print('·' * 20)
+termo = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+contador = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while contador <= total:
+        print('{}'.format(termo), end='')
+        print(' -> ' if contador < 10 else ' · ')
+        termo += razão
+        contador += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais?'))
+print('PA finalizada com {} termos mostrados'.format(total))
+print('Encerrando...')
--- pythonDesafios/des063.py
+++ pythonDesafios/des063.py
@@ -3,3 +3,20 @@
 Ex:
 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 '''
+# ? RESOLUÇÃO DESSA PORRA!
+
+print('-' * 30)
+print('SEQUÊNCIA DE FIBONACCI')
+print('-' * 30)
+n = int(input('Quantos termos você quer adicionar?'))
+t1 = 0
+t2 = 1
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(' -> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' | FIM.')
