55e73def582381a91bec531342c9778fa3d6d857
Dalton Borges <DaltonBorges@live.com>
2021-04-19 04:24:10 -0300

    Desafio 77 e 78 resolvidos

--- pythonDesafios/des078.py
+++ pythonDesafios/des078.py
@@ -1,15 +1,59 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
-#* No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+#* No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas cições na lista.
 #----------------------------------------------
-
+'''
 números = []
 for num in range(0, 5):
-    números.append(int(input('Digite um número: ')))
-print(f'O valor máximo digitado foi {max(números)}')
-print(f'O valor múnimo digitado foi: {min(números)}.')
+    números.append(int(input(f'Digite um número para a cição {num}: ')))
+print(f'Os números digitados foram {números}.')
+print(f'O vmaior valor digitado foi {max(números)}')
+print(f'O menor digitado foi: {min(números)}.')
 
-
+'''
 #* Funcionou!
+#todo Esqueci das cições!
 #----------------------------------------------
     
-    
+#? Resolução:
+'''
+números = []
+mai = 0
+men = 0
+for c in range(0, 5):
+    números.append(int(input(f'Digite um número para a posição {c}: ')))
+    if c == 0:
+        mai = men = números[c]
+    else:
+        if números[c] > mai:
+            mai = números[c]
+        if números[c] < men:
+            men = números[c]
+print('-='*30)
+print(f'Você digitou os valores {números}.')
+print(f'O maior valor digitado foi o {mai} nas posições', end = ' ')
+for i, v in enumerate(números):
+    if v == mai:
+        print(f'{i}...', end = ' ')
+print(f'O menor valor digitado foi o {men} nas posições', end = ' ')
+for i, v in enumerate(números):
+    if v == men:
+        print(f'{i}...', end = ' ')
+print()
+'''
+
+#? Opção simplificada:
+
+lista = []
+posicao_maior = []
+posicao_menor = []
+for p in range(0, 5):
+    val = int(input(f'Digite um valor na posição {p}: '))
+    lista.append(val)
+for posicao, valores in enumerate(lista):
+    if valores == max(lista):
+        posicao_maior.append(posicao)
+    if valores == min(lista):
+        posicao_menor.append(posicao)
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor da lista é {max(lista)}, nas posições {posicao_maior}')
+print(f'O menor valor da lista é {min(lista)}, nas posições {posicao_menor}')
--- pythonDesafios/des079.py
+++ pythonDesafios/des079.py
@@ -1,1 +1,16 @@
 # Crie um programa onde o usuário posa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidas todos os valores únicos digitados, em ordem crescente.
+#----------------------------------------------
+
+lista = []
+while True:
+    num = lista.append(int(input('Digite um número: ')))    
+    while num in lista:
+        print(f'O número {num} já se encontra na lista. Não será adicionado.')
+    segue = str(input('Deseja continuar? [S/N]')).strip().upper()                
+    while segue not in 'SN':
+        print('Opção inválida.')
+        segue = str(input('Deseja continuar? [S/N]')).strip().upper()
+    if segue == 'N':
+        break
+print(sorted(lista))
+            
